a86aa9f39f9e26ab72a7d63a4b35c9305b2dd6bd
Nikita Antonov <nikita.antonov2@leroymerlin.ru>
2023-12-29 01:49:06 +0300

    update

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -7,7 +7,7 @@
     </div>
 </template>
 <script setup lang="ts">
-// import Header from './Header/Header.vue';
+import Header from './Header/Header.vue';
 import { useUserData } from '../hooks/user-data';
 
 // const { getUsername } = useUserData()
