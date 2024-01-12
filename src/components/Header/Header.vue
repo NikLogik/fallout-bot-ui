25d64bdd7d979e59b6e7e793e7deaa3a634dc669
Nikita Antonov <nikita.antonov2@leroymerlin.ru>
2024-01-12 18:56:52 +0300

    test router

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -1,16 +1,18 @@
 <template>
     <div class="header-tg">
+        <BackButton/>
         <CloseButton/>
     </div>
 </template>
 <script setup lang="ts">
+import BackButton from '../buttons/BackButton.vue';
 import CloseButton from '../buttons/CloseButton.vue';
 </script>
 <style scoped>
 .header-tg {
     display: flex;
-    flex-direction: column;
-    justify-content: flex-end;
+    flex-direction: row;
+    justify-content: space-between;
     align-items: flex-end;
     margin-bottom: 0.5rem;
 }
